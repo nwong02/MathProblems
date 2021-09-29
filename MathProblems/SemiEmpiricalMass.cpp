2d5bb2d8341865b64d3222cf24a8258f5eb7fb53
Nelson Wong <nelsonwong107@gmail.com>
2021-09-29 12:32:01 -0400

    Corrected square root and cube roots, answer is corrected

--- MathProblems/SemiEmpiricalMass.cpp
+++ MathProblems/SemiEmpiricalMass.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 double nuclearBinding(int massNumber, int atomicNumber)
 {
+	double bindingEnergy = 0.0;
 	double a_1 = 15.67;
 	double a_2 = 17.23;
 	double a_3 = 0.75;
 	double a_4 = 93.2;
 	double a_5 = 0.0;
 
-	double bindingEnergy = 0.0;
 
-	if (massNumber % 2 == 0)
+	if (massNumber % 2 != 0)
 	{
 		a_5 = 0.0;
 	}
@@ -27,11 +27,12 @@ double nuclearBinding(int massNumber, int atomicNumber)
 	}
 
 	double firstTerm = a_1 * massNumber;
-	double secondTerm = a_3 * pow(atomicNumber, 2) / pow(massNumber, (1.0 / 3.0));
-	double thirdTerm = a_4 * pow(massNumber - 2 * atomicNumber, 2) / massNumber;
-	double fourthTerm = a_5 / pow(massNumber, (1.0 / 2.0));
+	double secondTerm = a_2 * pow(cbrt(massNumber), 2);
+	double thirdTerm = a_3 * pow(atomicNumber, 2) / cbrt(massNumber);
+	double fourthTerm = a_4 * pow(massNumber - 2 * atomicNumber, 2) / massNumber;
+	double fifthTerm = a_5 / pow(massNumber, 1.0 / 2.0);
 	
-	bindingEnergy = firstTerm - secondTerm - thirdTerm + fourthTerm;
+	bindingEnergy = firstTerm - secondTerm - thirdTerm - fourthTerm + fifthTerm;
 	cout << "The binding energy is: " << bindingEnergy;
 	return bindingEnergy;
 }
@@ -40,7 +41,7 @@ double nuclearBinding(int massNumber, int atomicNumber)
 int main()
 {
 	double A = 0.0;
-	double Z = 0;
+	double Z = 0.0;
 
 	cout << "Enter the mass number: ";
 	cin >> A;
@@ -49,6 +50,14 @@ int main()
 	cin >> Z;
 
 	nuclearBinding(A, Z);
+
+	/*double a = 16.0;
+	double squareA = pow(a, 2);
+	double rootA = pow(a, 1.0 / 2.0);
+	double cubeRootA = cbrt(a);
+	cout << squareA << endl;
+	cout << rootA << endl;
+	cout << cubeRootA << endl;*/
 
 	return 1;
 }
